2b51953d5d12ae96bf0c259fde6d249876b1e8a6
Pratiti Das <pratitidas10.1.92@gmail.com>
2024-01-02 17:47:14 -0800

    Flow Control

---
+++ FlowControl/RevNum.java
@@ -0,0 +1,21 @@
+package FlowControl;
+
+import java.util.*;
+
+public class RevNum {
+    
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+
+        int num = sc.nextInt();
+        int rev = 0;
+        while(num>0){
+            rev = (rev*10)+(num%10);
+            num /=10;
+        }
+
+        System.out.println("reverse:"+rev);
+    }
+}

e1662559cc150e955bda4e2ff51b658636ad2df6
Pratiti Das <pratitidas10.1.92@gmail.com>
2024-02-05 18:41:48 -0800

    Initial commit

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+import java.util.*;
+
+public class Pattern1{
+
+    public static void main(String args[]){
+
+        int line = 5;
+        for(int i = 1; i <= line; i++){
+            for(int j = 1; j <= i; j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
